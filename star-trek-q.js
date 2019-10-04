859952882674fce5b6e20fab1bd2097a64b2c98c
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-04 15:18:19 -0400

    dll queue

--- star-trek-q.js
+++ star-trek-q.js
@@ -1,4 +1,5 @@
 const Queue = require('./queue');
+const DLLQueue = require('./DLLqueue');
 const queueHelpers = require('./queueHelpers');
 
 function makeStarTrekQ() {
@@ -8,9 +9,18 @@ function makeStarTrekQ() {
   return starTrekQ;
 }
 
+function makeStarTrekDLLQ() {
+  const starTrekQ = new DLLQueue();
+  const chars = ['Kirk', 'Spock', 'Uhura', 'Sulu', 'Checkov'];
+  chars.forEach(char => starTrekQ.enqueue(char));
+  return starTrekQ;
+}
+
 const trek = makeStarTrekQ();
+const trekDLL = makeStarTrekDLLQ();
 
-trek.dequeue();
-trek.dequeue();
+// trek.dequeue();
+// trek.dequeue();
 
-queueHelpers().displayAll(trek);
+// queueHelpers().displayAll(trek);
+queueHelpers().displayAll(trekDLL);
