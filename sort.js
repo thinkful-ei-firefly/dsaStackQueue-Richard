130e049bd0d0d6b187de4d0a373baae34e83f989
rjgates3 <gates.j.richard@gmail.com>
2019-10-04 12:55:24 -0600

    work on sort stack

--- sort.js
+++ sort.js
@@ -1,43 +1,70 @@
+'use strict';
+
 const Stack = require('./stack');
 const stackHelpers = require('./stackHelpers');
-const display = stackHelpers().displayAll;
 
-function sortStack(stack) {
-  const tempStack = new Stack();
-  let curr = stack.top;
-  let temp = stack.pop();
-  while (curr.next) {
-    if (!tempStack.top) {
-      tempStack.push(temp)
-    } else if (temp > tempStack.top.data) {
-      result.push(stack.pop());
-    } else {
-      
+//input stack to be sorted smallest on top
+//temp stack to be sorted largest on top
+function sortStack(stackA) {
+    const tempStack = new Stack();
+    let temp = stackA.pop();
+    let curr = stackA.top;
+    while(curr !== null) {
+        //compare temp and curr
+        if(temp < curr) {
+            //push curr into tempStack
+
+        }
+      //pop curr to temp
+      //if temp > curr 
+      //pop off stackA push into tempStack
+      //get new curr
+      //end case when curr is null
     }
-    curr = curr.next;
-  }
-  while
-  return stack;
+
+  
+    return stackA;
 }
 
-function makeDisorder() {
-  const stack = new Stack();
-  const items = [1, 2, 10, 3, 5];
-  items.forEach(item => stack.push(item));
-  return stack;
+function makeDisorder(items) {
+    const stack = new Stack();
+    items.forEach(item => stack.push(item));
+    return stack;
 }
 
-const disorder = makeDisorder();
-// display(disorder);
-const sort = sortStack(disorder);
-// display(sort);
+const items = [1, 2, 10, 3, 5];
+const disorder = makeDisorder(items);
+stackHelpers.display(disorder);
+stackHelpers.display(sortStack(disorder));
 
 /*
+    max   min
+    11  
+A               B
 
-5      
-3
-10
-2
-1      null
+                
+                
+                5
+                3
+                2
+10                1
+
+
+
+
+null
+
+//check Max against currA
+//if currA is smaller           //if currA is larger          
+//check currA against currB     //Max push to B
+                                //currA pop to Max
+
+//if currA smaller              //if currA is larger           
+//currA in a Min                //pop currA push to B
+
+
+//go backwards until currA > currB
+//go forwards again
+
 
 */
